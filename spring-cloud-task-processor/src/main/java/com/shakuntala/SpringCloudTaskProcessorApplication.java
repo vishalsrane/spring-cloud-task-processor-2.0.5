c0aad049578ac41b22a7d569e33cec049cbf8be2
CatherineGeorgePres <g.cathy.g@gmail.com>
2018-10-08 01:02:46 +0530

    initial commit

---
+++ spring-cloud-task-processor/src/main/java/com/shakuntala/SpringCloudTaskProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.shakuntala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudTaskProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudTaskProcessorApplication.class, args);
+	}
+}
